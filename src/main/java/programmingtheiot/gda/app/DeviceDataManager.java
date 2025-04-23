927267975e6ea29a5163e383a8a835306e92708d
luciaavivero <luciaavivero@gmail.com>
2025-04-23 22:02:13 +0200

    PIOT-GDA-07-004

--- src/main/java/programmingtheiot/gda/app/DeviceDataManager.java
+++ src/main/java/programmingtheiot/gda/app/DeviceDataManager.java
@@ -30,6 +30,7 @@ import programmingtheiot.gda.connection.IRequestResponseClient;
 import programmingtheiot.gda.connection.MqttClientConnector;
 import programmingtheiot.gda.connection.RedisPersistenceAdapter;
 import programmingtheiot.gda.connection.SmtpClientConnector;
+import programmingtheiot.gda.system.SystemPerformanceManager;
 
 /**
  * Shell representation of class for student implementation.
@@ -49,6 +50,7 @@ public class DeviceDataManager implements IDataMessageListener
 	private boolean enableCloudClient = false;
 	private boolean enableSmtpClient = false;
 	private boolean enablePersistenceClient = false;
+	private boolean enableSystemPerf = false;
 	
 	private IActuatorDataListener actuatorDataListener = null;
 	private IPubSubClient mqttClient = null;
@@ -56,6 +58,8 @@ public class DeviceDataManager implements IDataMessageListener
 	private IPersistenceClient persistenceClient = null;
 	private IRequestResponseClient smtpClient = null;
 	private CoapServerGateway coapServer = null;
+	private SystemPerformanceManager sysPerfMgr = null;
+
 	
 	// constructors
 	
@@ -63,6 +67,7 @@ public class DeviceDataManager implements IDataMessageListener
 	{
 		super();
 		
+		initManager();
 		initConnections();
 	}
 	
@@ -117,10 +122,65 @@ public class DeviceDataManager implements IDataMessageListener
 	
 	public void startManager()
 	{
+		if (this.mqttClient != null) {
+			if (this.mqttClient.connectClient()) {
+				_Logger.info("Successfully connected MQTT client to broker.");
+	
+				// add necessary subscriptions
+	
+				// TODO: read this from the configuration file
+				int qos = ConfigConst.DEFAULT_QOS;
+	
+				// TODO: check the return value for each and take appropriate action
+	
+				// IMPORTANT NOTE: The 'subscribeToTopic()' method calls shown
+				// below will be moved to MqttClientConnector.connectComplete()
+				// in Lab Module 10. For now, they can remain here.
+				this.mqttClient.subscribeToTopic(ResourceNameEnum.GDA_MGMT_STATUS_MSG_RESOURCE, qos);
+				this.mqttClient.subscribeToTopic(ResourceNameEnum.CDA_ACTUATOR_RESPONSE_RESOURCE, qos);
+				this.mqttClient.subscribeToTopic(ResourceNameEnum.CDA_SENSOR_MSG_RESOURCE, qos);
+				this.mqttClient.subscribeToTopic(ResourceNameEnum.CDA_SYSTEM_PERF_MSG_RESOURCE, qos);
+			} else {
+				_Logger.severe("Failed to connect MQTT client to broker.");
+	
+				// TODO: take appropriate action
+			}
+		}
+	
+		if (this.sysPerfMgr != null) {
+			this.sysPerfMgr.startManager();
+		}
 	}
 	
 	public void stopManager()
 	{
+		if (this.sysPerfMgr != null) {
+			this.sysPerfMgr.stopManager();
+		}
+	
+		if (this.mqttClient != null) {
+			// add necessary un-subscribes
+	
+			// TODO: check the return value for each and take appropriate action
+	
+			// NOTE: The unsubscribeFromTopic() method calls below should match with
+			// the subscribeToTopic() method calls from startManager(). Also, the
+			// unsubscribe logic below can be moved to MqttClientConnector's
+			// disconnectClient() call PRIOR to actually disconnecting from
+			// the MQTT broker.
+			this.mqttClient.unsubscribeFromTopic(ResourceNameEnum.GDA_MGMT_STATUS_MSG_RESOURCE);
+			this.mqttClient.unsubscribeFromTopic(ResourceNameEnum.CDA_ACTUATOR_RESPONSE_RESOURCE);
+			this.mqttClient.unsubscribeFromTopic(ResourceNameEnum.CDA_SENSOR_MSG_RESOURCE);
+			this.mqttClient.unsubscribeFromTopic(ResourceNameEnum.CDA_SYSTEM_PERF_MSG_RESOURCE);
+	
+			if (this.mqttClient.disconnectClient()) {
+				_Logger.info("Successfully disconnected MQTT client from broker.");
+			} else {
+				_Logger.severe("Failed to disconnect MQTT client from broker.");
+	
+				// TODO: take appropriate action
+			}
+		}
 	}
 
 	
@@ -133,6 +193,39 @@ public class DeviceDataManager implements IDataMessageListener
 	 */
 	private void initConnections()
 	{
+	}
+
+	private void initManager()
+	{
+		ConfigUtil configUtil = ConfigUtil.getInstance();
+
+		this.enableSystemPerf =
+			configUtil.getBoolean(ConfigConst.GATEWAY_DEVICE,  ConfigConst.ENABLE_SYSTEM_PERF_KEY);
+
+		if (this.enableSystemPerf) {
+			this.sysPerfMgr = new SystemPerformanceManager();
+			this.sysPerfMgr.setDataMessageListener(this);
+		}
+
+		// NOTE: This is new - creating the MQTT client connector instance
+		if (this.enableMqttClient) {
+			this.mqttClient = new MqttClientConnector();
+
+			// NOTE: The next line isn't technically needed until Lab Module 10
+			this.mqttClient.setDataMessageListener(this);
+		}
+
+		if (this.enableCoapServer) {
+			// TODO: implement this in Lab Module 8
+		}
+
+		if (this.enableCloudClient) {
+			// TODO: implement this in Lab Module 10
+		}
+
+		if (this.enablePersistenceClient) {
+			// TODO: implement this as an optional exercise in Lab Module 5
+		}
 	}
 	
 }
